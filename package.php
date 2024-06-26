83493ada730d4c9554e863c1f73da682f29e3123
manvimahe <manvimahe@gmail.com>
2024-06-26 16:20:41 +0530

    adding package table

--- package.php
+++ package.php
@@ -15,6 +15,32 @@
 </head>
 <body>
 
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "book_db";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT title, description, image_path, button_text, button_link FROM packages";
+    $result = $conn->query($sql);
+
+    $packages = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $packages[] = $row;
+        }
+    }
+    $conn->close();
+    ?>
+
 
     <!-- header section start -->
 
@@ -41,150 +67,29 @@
         <h1>Packages</h1>
     </div>
 
-    <!-- packages section start -->
-    <section class="packages">
+    <!-- packages section starts -->
+<section class="packages">
+    <h1 class="heading-title">Top Destinations</h1>
 
-        <h1 class="heading-title">Top Destinations</h1>
-
-        <div class="box-container">
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
+    <div class="box-container">
+        <?php foreach ($packages as $package): ?>
+        <div class="box">
+            <div class="image">
+                <img src="<?php echo htmlspecialchars($package['image_path']); ?>" alt="">
             </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
-            </div>
-
-            <div class="box">
-                <div class="image">
-                    <img src="images/home-slide-21.jpeg" alt="">
-                </div>
-                <div class="content">
-                    <h3>adventure & tour</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad officiis rem a alias.</p>
-                    <a href="book.php" class="btn">Book now</a>
-                </div>
+            <div class="content">
+                <h3><?php echo htmlspecialchars($package['title']); ?></h3>
+                <p><?php echo htmlspecialchars($package['description']); ?></p>
+                <a href="<?php echo htmlspecialchars($package['button_link']); ?>" class="btn"><?php echo htmlspecialchars($package['button_text']); ?></a>
             </div>
         </div>
+        <?php endforeach; ?>
+    </div>
 
-        <div class="load-more"><span class="btn">load more</span></div>
-        
-    </section>
-    <!-- packages section ends -->
+    <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+<!-- packages section ends -->
 
 
 
